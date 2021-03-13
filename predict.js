504a8f961eb95b008694b9b0ad0a0b08d55b1d45
Long Vu <thienlong14th@gmail.com>
2021-03-12 19:36:41 -0600

    update t-diagram into predict.js

--- predict.js
+++ predict.js
@@ -5,10 +5,17 @@ function countFunc(){
     console.log(num);
     }
 }
-
-
-
-/* Answer: (0,1,2,3,4,5,6,7,8,9,10,11,12,13,14) */
+/* t-diagram
+num < 15 = num
+num
+0,0
+1,1
+2,2
+3,3
+...
+14,14
+15 break!!
+Answer: (0,1,2,3,4,5,6,7,8,9,10,11,12,13,14) */
 
 /* predict 2 */
 function oddFunc(){
@@ -19,20 +26,45 @@ function oddFunc(){
     }
 }
 }
-/*Answer: (3,9)*/
+/* t-diagram
+if x= (1,2,4,5,6,7,8,); i !== 0
+=>
+x,i
+3,0
+9,0
+
+Answer: (3,9)*/
 
 /* Predict 3 */
 function ifElseFunc(){
-    for(var j = 1; j <= 15; j++){
+    for(var j = 1; j <= 15; j++){ 
         if(j % 2 == 0){
             j+=2;
         }
         else if(j % 3 == 0){
             j++;
         }
-        alert(j)
+            alert(j)
         console.log(j);
     }
 }
-/* Answer: (1,4,5,8,10,11,14,16) */
+/* t-diagram
+so let's do the if loops:
+if(j % 2 == 0){ j+=2}
+j  =  value
+1  => 1; 
+j++ = 2 =>
+2+2 = 4;
+ j++ = 5;
+ 5 + 2 = 7, 7 !== 0
+ j++ = 8;
+ j++ = 9 (% 3 == 0)
+ j++ = 10;
+ j++ = 11; 
+ 11 + 2 = 13, 13 !== 0
+ j++ = 14;
+ 14 + 2 = 16;
+ 
+=> 1, 4, 5, 8, 10, 11, 14, 16
+Answer: (1,4,5,8,10,11,14,16) */
 
